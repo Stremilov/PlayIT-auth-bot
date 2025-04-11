42554a490e37ae8ab726d37fc42b30b1ef034915
Lef <116380867+Stremilov@users.noreply.github.com>
2025-04-11 13:32:21 +0300

    commit

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import asyncio
 import logging
 import os
 from datetime import datetime
+from sqlalchemy import text
+
 
 import pandas as pd
 from apscheduler.schedulers.asyncio import AsyncIOScheduler
@@ -10,6 +12,9 @@ from aiogram.filters import Command
 from aiogram import Bot, Dispatcher, F
 from aiogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton, WebAppInfo
 from dotenv import load_dotenv
+from sqlalchemy.orm import Session
+
+from db import get_db_session
 
 load_dotenv()
 
@@ -46,25 +51,35 @@ async def moder_button(message: Message):
 
 
 @dp.message(F.text == "rassilka_52")
-async def rass(message: Message):
-    df = pd.read_csv(CSV_FILE_PATH)
-    for username in df['telegram_username']:
-        try:
-            keyboard = InlineKeyboardMarkup(inline_keyboard=[
-                [InlineKeyboardButton(text="Открыть веб-приложение", web_app=WebAppInfo(url=WEB_APP_URL))]
-            ],
-                resize_keyboard=True
-            )
-            user = await bot.get_chat(f"{username}")
-            await bot.send_message(
-                chat_id=user.id,
-                text="Приветствую тебя, добрый молодец или девица красная! "
-                     "Дозволь уведомить о прибавлении новых заданий мудрёных.",
-                reply_markup=keyboard
-            )
-        except Exception as e:
-            logging.warning(f"Не удалось отправить сообщение @{username}: {e}")
-            logging.error(f"Ошибка при рассылке сообщений: {e}")
+async def rassilka_from_db_sql(message: Message):
+    session = next(get_db_session())
+    try:
+        result = session.execute(text("SELECT telegram_id FROM users WHERE telegram_id IS NOT NULL"))
+        telegram_ids = [row[0] for row in result.fetchall()]
+
+        logging.info(f"Всего пользователей для рассылки: {len(telegram_ids)}")
+
+        keyboard = InlineKeyboardMarkup(inline_keyboard=[
+            [InlineKeyboardButton(text="Открыть веб-приложение", web_app=WebAppInfo(url=WEB_APP_URL))]
+        ],
+            resize_keyboard=True
+        )
+
+        for tg_id in telegram_ids:
+            try:
+                await bot.send_message(
+                    chat_id=tg_id,
+                    text="Приветствую тебя, добрый молодец или девица красная! "
+                         "Дозволь уведомить о прибавлении новых заданий мудрёных.",
+                    reply_markup=keyboard
+                )
+                await asyncio.sleep(0.05)
+            except Exception as e:
+                logging.warning(f"Не удалось отправить сообщение {tg_id}: {e}")
+    except Exception as e:
+        logging.error(f"Ошибка при обращении к базе данных: {e}")
+    finally:
+        session.close()
 
 @dp.message(Command("start"))
 async def start_cmd(message: Message):
